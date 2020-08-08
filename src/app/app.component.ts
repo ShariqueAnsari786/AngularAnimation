59c63bdd3c2e00580d0d8dc78e935fe7a5b1e54f
Sharique <shariqueansariii@gmail.com>
2020-08-09 01:31:09 +0530

    [CODE] All the angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: { class: "mat-typography" }
 })
-export class AppComponent {
-  title = 'AnimationEffects';
+export class AppComponent { 
+
+  public home = true;
+
+  public reload() {
+
+    this.home = false;
+
+    setTimeout(() => this.home = true);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,70 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { IconComponent } from './icon/icon.component';
+import { AnimateModule } from './animate/animate.module';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports:      [   
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    // ShareModule.forRoot(),
+    // FontAwesomeModule,
+    // FlexLayoutModule, 
+    ScrollingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatProgressBarModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatButtonToggleModule,
+    AnimateModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  
+  declarations: [ 
+    AppComponent, HomeComponent,
+    HomeComponent,
+    IconComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
